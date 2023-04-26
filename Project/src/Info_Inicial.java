ae147179b6f393dbea60147da8cc1dd5d0208c1c
JoaoGDFaria <joaofaria13@gmail.com>
2023-04-26 23:32:56 +0100

    Criação de malas e tshirts

--- Project/src/Info_Inicial.java
+++ Project/src/Info_Inicial.java
@@ -31,11 +31,56 @@ public class Info_Inicial {
 
 
     // TSHIRTS
+    Artigo t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,t13,t14,t15,t16,t17,t18,t19,t20;
+    t1 = new Tshirts("T-shirt preta", "Reebok", "RE-30", 72.05, 'N', 4, "FedEx", 'X', 'R');
+    t2 = new Tshirts("T-shirt branca", "Fila", "FI-23", 99.66, 'N', 0, "DHL", 'L', 'P');
+    t3 = new Tshirts("T-shirt rosa", "Adidas", "AD-4", 57.50, 'U', 9, "FedEx", 'X', 'L');
+    t4 = new Tshirts("T-shirt branca", "New Balance", "NE-69", 78.99, 'N', 4, "Correios", 'S', 'L');
+    t5 = new Tshirts("T-shirt verde", "Fila", "FI-62", 7.82, 'U', 9, "Correios", 'X', 'R');
+    t6 = new Tshirts("T-shirt azul", "Converse", "CO-50", 15.36, 'U', 9, "DHL", 'X', 'P');
+    t7 = new Tshirts("T-shirt branca", "Converse", "CO-45", 78.97, 'U', 0, "UPS", 'L', 'L');
+    t8 = new Tshirts("T-shirt amarela", "Converse", "CO-31", 62.97, 'U', 3, "FedEx", 'M', 'P');
+    t9 = new Tshirts("T-shirt roxa", "New Balance", "NE-43", 68.67, 'N', 6, "FedEx", 'X', 'L');
+    t10 = new Tshirts("T-shirt roxa", "Under Armour", "UN-100", 92.52, 'N', 3, "FedEx", 'S', 'P');
+    t11 = new Tshirts("T-shirt amarela", "Reebok", "RE-98", 69.95, 'N', 0, "Correios", 'S', 'P');
+    t12 = new Tshirts("T-shirt preta", "Converse", "CO-99", 76.95, 'U', 4, "USPS", 'M', 'L');
+    t13 = new Tshirts("T-shirt amarela", "Nike", "NI-65", 5.94, 'N', 1, "DHL", 'S', 'L');
+    t14 = new Tshirts("T-shirt laranja", "Reebok", "RE-57", 93.27, 'U', 6, "UPS", 'S', 'P');
+    t15 = new Tshirts("T-shirt amarela", "Converse", "CO-55", 41.76, 'U', 6, "DHL", 'S', 'R');
+    t16 = new Tshirts("T-shirt preta", "New Balance", "NE-78", 96.43, 'N', 4, "Correios", 'M', 'P');
+    t17 = new Tshirts("T-shirt vermelha", "New Balance", "NE-92", 70.28, 'N', 9, "USPS", 'X', 'R');
+    t18 = new Tshirts("T-shirt laranja", "Asics", "AS-61", 34.44, 'U', 1, "DHL", 'S', 'L');
+    t19 = new Tshirts("T-shirt preta", "Asics", "AS-16", 35.75, 'U', 1, "USPS", 'X', 'R');
+    t20 = new Tshirts("T-shirt azul", "Nike", "NI-96", 99.19, 'U', 8, "USPS", 'L', 'P');
+
 
 
 
 
     // MALAS
+    Artigo m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16, m17, m18, m19, m20;
+    m1 = new Malas("Mala grande", "Rimowa", "das3", 554.08, 'b', 8, "Fedex", 103, 38, 27, "Nylon", 2010, false);
+    m2 = new Malas("Mala grande", "Samsonite", "gafdas3", 983.98, 'a', 4, "UPS", 43, 29, 16, "Couro", 2020, false);
+    m3 = new Malas("Mala grande", "Delsey", "gfasd1", 1010.10, 'n', 8, "DHL", 66, 29, 21, "Couro", 1963, false);
+    m4 = new Malas("Mala de mão", "Rimowa", "gdsa4", 487.46, 'c', 3, "TNT", 83, 38, 55, "PVC", 2015, true);
+    m5 = new Malas("Mala de mão", "American Tourister", "bfahus1", 258.82, 'c', 4, "Fedex", 40, 41, 13, "ABS", 1977, true);
+    m6 = new Malas("Mala de viagem", "Samsonite", "313da", 471.24, 'c', 7, "USPS", 80, 39, 23, "PVC", 1963, true);
+    m7 = new Malas("Mala executiva", "Samsonite", "fasg1", 464.77, 'b', 9, "DHL", 64, 37, 15, "Nylon", 2022, true);
+    m8 = new Malas("Mala executiva", "Tumi", "gsad123", 513.25, 'n', 3, "TNT", 82, 45, 10, "Couro", 1963, true);
+    m9 = new Malas("Mala de rodinhas", "Rimowa", "fgcdx", 734.43, 'a', 5, "DHL", 95, 12, 42, "PVC", 1977, true);
+    m10 = new Malas("Mala de rodinhas", "American Tourister", "dasd4", 511.93, 'a', 0, "UPS", 118, 36, 12, "Nylon", 2022, true);
+    m11 = new Malas("Mala de mão", "Rimowa", "gads54", 633.61, 'n', 2, "Fedex", 101, 38, 57, "Couro", 1988, true);
+    m12 = new Malas("Mala executiva", "Tumi", "dashdas1", 833.13, 'c', 6, "TNT", 48, 44, 54, "Couro", 2010, true);
+    m13 = new Malas("Mala executiva", "American Tourister", "gas2", 788.35, 'c', 5, "Fedex", 69, 46, 28, "PVC", 2022, false);
+    m14 = new Malas("Mala grande", "Delsey", "411as", 1048.01, 'c', 0, "USPS", 25, 43, 52, "Nylon", 2010, false);
+    m15 = new Malas("Mala grande", "Delsey", "asdhj7", 273.01, 'c', 4, "TNT", 107, 55, 36, "Poliéster", 2022, true);
+    m16 = new Malas("Mala grande", "Samsonite", "jggh2", 110.75, 'a', 8, "Fedex", 74, 24, 47, "Couro", 2010, false);
+    m17 = new Malas("Mala executiva", "American Tourister", "3123j", 795.88, 'a', 1, "USPS", 39, 10, 29, "ABS", 2022, true);
+    m18 = new Malas("Mala de mão", "Delsey", "asddg", 876.98, 'a', 5, "UPS", 111, 29, 20, "PVC", 1999, false);
+    m19 = new Malas("Mala de viagem", "Tumi", "hjjyut1", 70.73, 'c', 4, "DHL", 79, 59, 33, "Nylon", 2020, true);
+    m20 = new Malas("Mala grande", "Tumi", "hgfhdf2", 103.96, 'b', 6, "UPS", 62, 57, 54, "PVC", 2020, true);
+
+
 
 
 
